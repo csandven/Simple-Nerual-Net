a30e3f98fc8b664013fb70e88bc156579e08b648
Flodnes <chs_91@hotmail.com>
2018-05-01 22:55:55 +0100

    initial commit

---
+++ nn/network.js
@@ -0,0 +1,123 @@
+
+import Neuron from './neuron'
+
+export default class Network {
+
+	constructor (layers) {
+		if (layers instanceof Network) {
+			this.neurons = Object.assign([], layers.neurons)
+		} else {
+			this.neurons = []
+
+			// Setup the layers
+			for (var i = 0; i < layers.length; i++) {
+				for (var j = 0; j < layers[i]; j++) {
+					this.neurons[i] = this.neurons[i] || []
+					this.neurons[i].push(new Neuron(i))
+				}
+			}
+
+			// Setup the weights based on the previous layer
+			for (var i = 1; i < layers.length; i++) {
+				for (var j = 0; j < layers[i]; j++) {
+					this.neurons[i][j].initializeWeights(this.neurons[i - 1])
+				}
+			}
+		}
+	}
+
+	addHiddenLayer () {
+		const outputLayer = this.neurons.splice(-1),
+			  newNeuron = new Neuron(this.neurons.length)
+		this.neurons[this.neurons.length] = []
+		this.neurons[this.neurons.length - 1].push(newNeuron)
+		this.recalculateWeigths(this.neurons.length - 1)
+		this.neurons[this.neurons.length] = outputLayer[0]
+		this.recalculateWeigths(this.neurons.length - 1)
+	}
+
+	addNeuron (layer) {
+		const neuron = new Neuron(layer)
+		neuron.initializeWeights(this.neurons[layer - 1])
+		this.neurons[layer].push(neuron)
+		this.recalculateWeigths(layer + 1)
+	}
+
+	recalculateWeigths (layer) {
+		for (var i = 0; i < this.neurons[layer].length; i++) {
+			this.neurons[layer][i].initializeWeights(this.neurons[layer - 1])
+		}
+	}
+
+	input (...args) {
+		if (args.length !== this.neurons[0].length)
+			throw new Error('Number of passed inputs must match the number of input neurons')
+
+		for (var i = 0; i < this.neurons[0].length; i++) {
+			this.neurons[0][i].output = args[i]
+		}
+
+		for (var n = 1; n < this.neurons.length; n++) {
+			for (var i = 0; i < this.neurons[n].length; i++) {
+				const inputs = this.getLayerOutput(this.neurons[n - 1])
+				this.neurons[n][i].setInput(inputs)
+				this.neurons[n][i].prepareOutput()
+			}
+		}
+	}
+
+	output () {
+		const lastLayer = this.neurons[this.neurons.length - 1]
+
+		const out = lastLayer.map(neuron => neuron.output)
+
+		// Clear the neuron outputs
+		for (var i = 0; i < this.neurons.length; i++) {
+			for (var j = 0; j < this.neurons[i].length; j++) {
+				this.neurons[i][j].output = null
+			}
+		}
+
+		return out
+	}
+
+	getLayerOutput (layer) {
+		if (!layer)
+			throw new Error('A valid layer needs to be passed to get the outputs')
+
+		var outputs = []
+		for (var i = 0; i < layer.length; i++) {
+			outputs.push(layer[i].output)
+		}
+		return outputs
+	}
+
+	copy () {
+		return new Network(this)
+	}
+
+	getJson () {
+		return this.neurons
+	}
+
+	fromJson (json) {
+		// Setup the layers
+		for (var i = 0; i < json.length; i++) {
+			for (var j = 0; j < json[i]; j++) {
+				this.neurons[i] = this.neurons[i] || []
+
+				const neuron = new Neuron(i)
+				neuron.setWeights(json[i].weights)
+				neuron.setBias(json[i].bias)
+				this.neurons[i].push(neuron)
+			}
+		}
+	}
+
+	static newNeuron (layerIndex, bird) {
+		const neuron = new Neuron(layerIndex)
+		neuron.initializeWeights(bird.brain.neurons[layerIndex - 1])
+		return neuron
+	}
+
+}
---
+++ nn/neuron.js
@@ -0,0 +1,63 @@
+import { random } from '../utilities/maths'
+
+export default class Neuron {
+	
+	constructor (layerIndex) {
+		this.layerIndex = layerIndex
+		this.weights = []
+		this.bias = 0
+
+		this.inputs = []
+		this.output = null
+	}
+
+	initializeWeights (layer) {
+		this.weights = []
+		for (var i = 0; i < layer.length; i++) {
+			this.weights.push(+random(-2, 2).toFixed(2))
+		}
+	}
+
+	setInput (...args) {
+		this.inputs = args[0]
+	}
+
+	prepareOutput () {
+		if (this.inputs.length !== this.weights.length)
+			throw new Error('Number of passed inputs must match the number neurons weights')
+
+		var values = []
+		for (var i = 0; i < this.inputs.length; i++) {
+			values.push(this.inputs[i] * this.weights[i])
+		}
+
+		this.output = this.sigmoid(values.reduce((a,b) => a + b, 0) + this.bias)
+	}
+
+	normalizeInputs (inputs) {
+		const inSum = inputs.reduce((a,b) => a + b, 0)
+
+		var newValues = []
+		for (var i = 0; i < inputs.length; i++) {
+			newValues.push(inputs[i] / inSum)
+		}
+		
+		return newValues
+	}
+	
+	setWeights (weights) {
+		this.weights = []
+		for (var i = 0; i < weights.length; i++) {
+			this.weights.push(weights[i])
+		}
+	}
+
+	setBias (bias) {
+		this.bias = bias
+	}
+
+	sigmoid (t) {
+	    return 1/(1+Math.exp(-t))
+	}
+
+}
